4dc820bdda5e37f077f36e9b36e96706a6050013
kharisovilyas <91402961+kharisovilyas@users.noreply.github.com>
2023-11-17 01:49:09 +0300

    фÐadd server! On spring

---
+++ SortingAlgorithmsServer/src/main/java/com/example/SortingAlgorithmsServer/SortingAlgorithmsServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.SortingAlgorithmsServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortingAlgorithmsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortingAlgorithmsServerApplication.class, args);
+	}
+
+}
